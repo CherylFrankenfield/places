7d549ede8d2b63f7ce38d894530f4d5eb2b20d07
Cheryl Frankenfield and luke bertram <luke@>
2017-10-30 11:48:03 -0700

    Add name time and description attributes

--- js/scripts.js
+++ js/scripts.js
@@ -12,19 +12,23 @@ $(document).ready(function() {
   $("#newPlace").submit(function(event){
     event.preventDefault();
 
-    var userInput = $("#placeName").val();
-    var newPlace = new Place(userInput);
+    var inputName = $("#placeName").val();
+    var inputTime = $("#placeTime").val();
+    var inputDescription = $("#placeDescription").val();
+    var newPlace = new Place(inputName, inputTime, inputDescription);
 
     $("#place-list").append("<li><span class='place'>" + newPlace.name + "</span></li>");
 
     $(".place").last().click(function() {
       $("#outputPlace h2").text(newPlace.name);
-      // $("#placeImage").attr('src','http://www.placecage.com/g/200/300')
-      $("#outputPlace p.time").text("it was summer");
-      $("#outputPlace p.description").text("Oh my gosh. You'll never believe it. It was so great and you'll never experience it for yourself, because it's gone now. I was the last one to go there.")
+      $("#placeImage").attr('src','http://www.placecage.com/g/200/300')
+      $("#outputPlace p.time").text(newPlace.time);
+      $("#outputPlace p.description").text(newPlace.description);
     });
 
     $("#placeName").val("");
+    $("#placeTime").val("");
+    $("#placeDescription").val("");
 
   });
 });
