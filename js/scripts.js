d6bea2d8d218b6a2b3802a1db1f00b1129501c56
Cheryl Frankenfield and luke bertram <luke@>
2017-10-30 14:03:08 -0700

    Add image URL attribute to place constructor

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,16 @@ function Place(name, time, description) {
   this.name = name;
   this.time = time;
   this.description = description;
+  this.imageUrl = "";
 }
 
+Place.prototype.placeAndTime = function(){
+  return this.name + " in " + this.time;
+};
 
+Place.prototype.addImageLocation = function(url){
+  this.imageUrl = url;
+};
 
 //front-end foolery
 $(document).ready(function() {
@@ -16,11 +23,13 @@ $(document).ready(function() {
     var inputTime = $("#placeTime").val();
     var inputDescription = $("#placeDescription").val();
     var newPlace = new Place(inputName, inputTime, inputDescription);
+    var image = $("#imageUrl").val();
+    newPlace.addImageLocation(image);
 
     $("#place-list").append("<li><span class='place'>" + newPlace.name + "</span></li>");
 
     $(".place").last().click(function() {
-      $("#outputPlace h2").text(newPlace.name);
+      $("#outputPlace h2").text(newPlace.placeAndTime());
       $("#placeImage").attr('src','http://www.placecage.com/g/200/300')
       $("#outputPlace p.time").text(newPlace.time);
       $("#outputPlace p.description").text(newPlace.description);
